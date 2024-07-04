765e0d2542fcb83c8f1e36cc6cc76fd69d7b2fd9
Jaíne Rosa <jaine.rosa_11@live.com>
2024-07-04 19:35:46 -0300

    feat(categoria): implementando servico CRUD

--- crud-jpa/src/main/java/com/adas/crud_jpa/controller/CategoriaController.java
+++ crud-jpa/src/main/java/com/adas/crud_jpa/controller/CategoriaController.java
@@ -3,6 +3,7 @@ package com.adas.crud_jpa.controller;
 import com.adas.crud_jpa.model.Categoria;
 import com.adas.crud_jpa.service.CategoriaService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -15,15 +16,22 @@ public class CategoriaController {
     private CategoriaService categoriaService;
 
     @PostMapping
-    public void cadastrar(@RequestBody Categoria categoria){
-
-            categoriaService.salvar(categoria);
-
+    public Categoria cadastrar(@RequestBody Categoria novaCategoria){
+        return categoriaService.salvar(novaCategoria);
     }
-
     @GetMapping()
     public List<Categoria> buscarTodos(){
             return  categoriaService.buscarTodos();
     }
 
+    @GetMapping("/{id}")
+    public  ResponseEntity<Categoria> buscarPorId(@PathVariable int id){
+        Categoria categoriaEncontrada= categoriaService.buscarPorId(id);
+        if( categoriaEncontrada == null){
+            return ResponseEntity.notFound().build();
+
+        }
+        return ResponseEntity.ok(categoriaEncontrada);
+    }
 }
+
